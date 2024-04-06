fd62c67555358b3922a58710c23872e93e76aee9
Olav Rorvik <olavr.dev@gmail.com>
2024-04-06 10:38:12 +0200

    learning to use "this" and classes and methods

--- objects.js
+++ objects.js
@@ -14,10 +14,18 @@ class Job {
     this.location = location;
     this.salary = salary;
   }
+  // Adding your own method
+  describe() {
+    console.log(`I'm a ${this.title}, I work in ${this.location} and I earn ${this.salary}.`);
+  }
 }
 
 // You can use this blueprint to create new instances with the new keyword
 const developer = new Job('Web Developer', 'Sandnes', 750000);
-const cook = new Job('Cook', 'Stavanger', 450000);
 console.log(developer);
+const cook = new Job('Cook', 'Stavanger', 450000);
 console.log(cook);
+
+// Using the new method on the blueprint
+const pilot = new Job('Pilot', 'Oslo', 925000);
+pilot.describe();

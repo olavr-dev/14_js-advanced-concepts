5be5df7597467027aa3a052f315f812330f9d86b
Olav Rorvik <olavr.dev@gmail.com>
2024-04-08 12:20:04 +0200

    working with async / await

--- async.js
+++ async.js
@@ -1,36 +1,21 @@
 // Accessing the file system is back-end only (NodeJS).
-// const fs = require('fs');
 
 // fs/promises is a built in package.
 const fs = require('fs/promises');
 
-function readFile() {
+// The async keyword will always return a promise
+async function readFile() {
   let fileData;
-
-  // // Asynchronous task using a callback function
-  // This will display last, since it loads the file first
-  // fileData = fs.readFile('data.txt', function (error, fileData) {
-  //   console.log('File parsing done!');
-  //   console.log(fileData.toString());
-  // });
 
   // Since we call now call fs/promises it does no longer require a
   // callback function
 
   // This is a promise chain
-  fs.readFile('data.txt')
-    .then(function (fileData) {
-      console.log('File parsing done!');
-      console.log(fileData.toString());
-    })
-    // return anotherAsyncOperation
-    .then(function () {})
-    // error handling
-    .catch(function (error) {
-      console.log(error);
-    });
+  // The await keyword is used in front of any method that returns a promise
+  fileData = await fs.readFile('data.txt');
+  console.log('File parsing done!');
+  console.log(fileData.toString());
 
-  // Synchronous task. This will display first, while readFile is loading.
   console.log('Hello World!');
 }
 
